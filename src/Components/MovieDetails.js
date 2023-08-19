5f18dcd0d940e70d1d4022774d35441d548e897a
namrata <namratarana9@gmail.com>
2023-08-19 22:49:30 +0530

    details page added

--- src/Components/MovieDetails.js
+++ src/Components/MovieDetails.js
@@ -3,8 +3,17 @@ import React, {useState} from 'react';
 const MovieDetails = ()=>
 {
     return(
-        <div>
-            
+        <div className='details-container'>
+            <div>
+                <img 
+                className='poster'
+                src="https://image.tmdb.org/t/p/w220_and_h330_face/3IhGkkalwXguTlceGSl8XUJZOVI.jpg"
+                // src= {`${BASE_IMG_URL} ${poster_path}`} 
+                alt="loading"/>
+            </div>
+            <div>
+
+            </div>
         </div>
     )
 }
